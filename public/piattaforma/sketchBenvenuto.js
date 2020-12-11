5ba971d5809b7169b29cfbcc2d35c0ec5faa15ef
chiara <chiara7.brambilla@mail.polimi.it>
2020-12-11 16:26:54 +0100

    benvenuto +pagellino

--- public/piattaforma/sketchBenvenuto.js
+++ public/piattaforma/sketchBenvenuto.js
@@ -17,30 +17,33 @@ function setup() {
   image(logoIcon,  width/2 , height / 2, logoIcon.width/7, logoIcon.height/7);
   }
 /////////////////////////////////////////////////////////////////////////
-function mouseClicked() {
-  if (pag == 0) {
-    background('#F9F9F9'); //chiaro
-    image(benvenuto, width / 2, height / 50*28, benvenuto.width / 6, benvenuto.height / 6);
-  //testo caratteristiche
-  textFont('quicksand');
-  textAlign(CENTER, TOP);
-  textStyle(BOLD);
 
-  //testo centrale
-  textSize(27);
-  fill('#877B85'); //4° colore PALETTE
-  text('Benvenuto,', width/2, height / 50 *13);
-  fill('#B7AEB5'); //3° PALETTE
-  text('Dodicesimo Uomo',width / 2,  height / 50 *15);
-  textSize(13);
-  text('IL VERO TIFO',width / 2,  height / 50 *39);
-  button= createButton('inizia');
-  button.position(width/2-button.width/2,  height / 50*44);
-  button.mousePressed();
+function mouseClicked() {
+
+  if (pag == 0) {
+  //   background('#F9F9F9'); //chiaro
+  //   image(benvenuto, width / 2, height / 50*28, benvenuto.width / 6, benvenuto.height / 6);
+  // //testo caratteristiche
+  // textFont('quicksand');
+  // textAlign(CENTER, TOP);
+  // textStyle(BOLD);
+  //
+  // //testo centrale
+  // textSize(27);
+  // fill('#877B85'); //4° colore PALETTE
+  // text('Benvenuto,', width/2, height / 50 *13);
+  // fill('#B7AEB5'); //3° PALETTE
+  // text('Dodicesimo Uomo',width / 2,  height / 50 *15);
+  // textSize(13);
+  // text('IL VERO TIFO',width / 2,  height / 50 *39);
+  // button= createButton('inizia');
+  // button.position(width/2-button.width/2,  height / 50*44);
+  // button.mousePressed();
   } else if (pag == 1) {
-      window.open('indexPausa.html','_self');
+      window.open('benvenuto/index.html','_self');
       }
       pag++;
+
   }
   /////////////////////////////////////////////////////////////////////
 
