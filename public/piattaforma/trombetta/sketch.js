88702770f613fbddd914a558626d96ca44c2dd31
chiara <chiara7.brambilla@mail.polimi.it>
2020-12-18 12:09:38 +0100

    prove

--- public/piattaforma/trombetta/sketch.js
+++ public/piattaforma/trombetta/sketch.js
@@ -389,7 +389,7 @@ function draw() {
   console.log("daspo totale " + daspo_counter);
 
   ///////cambio cartella //////////////////////////////////////////////////
-  if (testo == 145 || testo < 145) {
+  if (testo == 133 || testo < 133) {
     window.open('../indexPausa.html', '_self'); //doppio puntino per andare nella cartella sopra
   }
   //////////////////////////////////////////////////////////////////
