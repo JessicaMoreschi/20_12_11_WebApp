78da30e41ae16bdbef26e1f0311d0715af2dc10c
JessicaMoreschi <jessica.moreschi98@gmail.com>
2020-12-19 18:28:16 +0100

    qr

--- public/piattaforma/assets/qrCode/sketch.js
+++ public/piattaforma/assets/qrCode/sketch.js
@@ -3,7 +3,7 @@ function preload(){
 }
 
 function setup() {
-  window.open('192.168.1.101:3000/piattaforma/benvenuto', "_top");
+  window.open('192.168.1.101:3000/piattaforma/benvenuto', "_self");
 }
 
 function draw() {
